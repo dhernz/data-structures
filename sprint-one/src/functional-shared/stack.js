74d682333cb701b063ea8d2ae6c65d0bc73ae03c
dhernz <d.hernz@gmail.com>
2015-07-02 16:35:24 -0700

    Passed the fourth test of functional

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,35 +1,80 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  return {
-  	push: stackMethods.push,
-  	pop: stackMethods.pop,
-  	size: stackMethods.size
-  }
+  	var obj = {};
+
+  	obj.storage = {};
+	obj.count = 0;
+  	//console.log(obj.count);
+  	obj.push = stackMethods.push;
+  	obj.pop = stackMethods.pop;
+  	obj.size = stackMethods.size;
+  
+
+
+
+  return obj;
 };
 
-var stackMethods = {
-	storage: {},	
+var stackMethods = {};
+stackMethods.push = function(value){ this.count++;};
+stackMethods.pop = function(){this.count--;};
+stackMethods.size = function(){return this.count;};
 
-	push: function(value){
-		this.size(1);
-	},
 
-	pop: function(){
 
-	},
 
-	size: function(item){
-		var count = 0;
-		// if(item === undefined){
-		// 	return count;
-		// }
-		// else{
-			return count += item;
-		// }
-		// return count;
-	}
 
-};
+
+
+
+
+// var newStack = Stack();
+// console.log(newStack);
+
+// newStack.push('a');
+// newStack.push('a');
+// newStack.pop();
+// console.log(newStack.size());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var stackMethods = {
+// 	storage: {},	
+
+// 	push: function(value){
+// 		this.size(1);
+// 	},
+
+// 	pop: function(){
+
+// 	},
+
+// 	size: function(item){
+// 		// var count = 0;
+// 		// if(item === undefined){
+// 		// 	return count;
+// 		// }
+// 		// else{
+// 			return count += item;
+// 	// 	}
+// 	// 	return count;
+// 	// }
+
+// };
 
 
