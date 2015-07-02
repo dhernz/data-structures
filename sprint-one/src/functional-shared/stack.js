084d5fca4200ab2f773603477c77f83061460d9e
dhernz <d.hernz@gmail.com>
2015-07-01 19:11:08 -0700

    Still working on functional shared pattern

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,35 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  return {
+  	push: stackMethods.push,
+  	pop: stackMethods.pop,
+  	size: stackMethods.size
+  }
 };
 
-var stackMethods = {};
+var stackMethods = {
+	storage: {},	
+
+	push: function(value){
+		this.size(1);
+	},
+
+	pop: function(){
+
+	},
+
+	size: function(item){
+		var count = 0;
+		// if(item === undefined){
+		// 	return count;
+		// }
+		// else{
+			return count += item;
+		// }
+		// return count;
+	}
+
+};
 
 
