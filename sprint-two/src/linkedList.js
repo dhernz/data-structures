1dd91a6c3976b29dc78dca2342b4fa3de05043f3
dhernz <d.hernz@gmail.com>
2015-07-03 12:34:39 -0700

    Done linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,16 +4,36 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var node = Node(value);
+    // console.log(node);
+    list.tail = node;
+
+    if(list.head === null){
+      list.head = list.tail;
+    }
+
+    // console.log(list.tail);
   };
 
   list.removeHead = function(){
+   if(list.head === null){
+    list.head = list.tail;
+   }
+
+    var removeHead = list.head;
+    list.head = list.tail;
+    //console.log('Removed head', removeHead);
+    return removeHead.value;
   };
 
   list.contains = function(target){
-  };
+  
+    return _.contains(list, target);
+      };
 
   return list;
 };
+
 
 var Node = function(value){
   var node = {};
@@ -23,6 +43,8 @@ var Node = function(value){
 
   return node;
 };
+
+
 
 /*
  * Complexity: What is the time complexity of the above functions?
