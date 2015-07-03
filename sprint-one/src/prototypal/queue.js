6f9e3901682ff10e6a8bd96a5772ef0d71a5c3ba
dhernz <d.hernz@gmail.com>
2015-07-02 21:34:07 -0700

    done with prototypal

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,50 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+
+var obj = Object.create(queueMethods);
+	obj.storage ={};
+	obj.storage.first = null;
+	obj.storage.second = null;
+	obj.count = 0;
+
+	return obj;
+
+
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function(value){
+	this.storage.value = value;
+	if(this.count === 0){
+		this.storage.second = this.storage.value;
+	}
+
+	this.storage.first = this.storage.second;
+	this.storage.second = this.storage.value;
+	this.count++;
+
+};
+
+queueMethods.dequeue = function(){
+	if(this.count === 0){
+		return this.count;
+	}
+	
+	this.count--;
+
+	var goodbye = this.storage.first;
+	this.storage.first = this.storage.second;
+	return goodbye;
+
+
+};
+
+queueMethods.size = function(){
+	return this.count;
+
+};
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,41 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(stackMethods);
+	obj.storage ={};
+	obj.storage.last = null;
+	obj.storage.first = null;
+	obj.count = 0;
+
+	return obj;
+
+
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value){
+	this.storage.value = value;
+	if(this.storage.last === null){
+		this.storage.last = this.storage.before;
+	}
+	this.storage.before = this.storage.last;
+	this.storage.last = this.storage.value;
+	this.count++;
+};
+
+stackMethods.pop = function(){
+	if(this.count === 0){
+		return this.count;
+	}
+	this.count--;
+	var seeYou = this.storage.last;
+	this.storage.last = this.storage.before;
+	return seeYou;
+};
+
+stackMethods.size = function(){
+	return this.count;
+
+};
 
