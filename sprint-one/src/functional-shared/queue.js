4af627a7b54e8283f601db1b4ba960bc3f948a55
dhernz <d.hernz@gmail.com>
2015-07-02 18:03:54 -0700

    Perosnal editing queue-functional

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,6 +1,18 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  	var obj = {}; 
+  	 	obj.count = 0;
+  	 	obj.storage = {};
+
+  	 	obj.push = queueMethods.push;
+  	 	obj.pop = queueMethods.pop;
+  	 	obj.size = queueMethods.size;
+
+  	 
+
+
+
 };
 
 var queueMethods = {};
