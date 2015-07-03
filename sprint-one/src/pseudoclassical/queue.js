00498ff6b36cb70f986a164a7569371726af0f1b
dhernz <d.hernz@gmail.com>
2015-07-02 21:58:05 -0700

    almost done this pseudoclassical

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,37 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.storage.first = null;
+  this.storage.second = null;
+  this.count = 0;
+
 };
+
+Queue.prototype.enqueue = function(value){
+	this.storage.value = value;
+	if(this.count === 0){
+		this.storage.second = this.storage.value;
+	}
+	this.storage.first = this.storage.second;
+	this.storage.second = this.storage.value;
+	this.count++;
+}
+
+Queue.prototype.dequeue = function(){
+	if(this.count ===0){
+		return this.count;
+	}
+	this.count--;
+	var seeYou = this.storage.first;
+	this.storage.first = this.storage.second;
+	return seeYou;
+	
+}
+
+Queue.prototype.size = function(){
+	return this.count;
+	
+}
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,33 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.storage.last = null;
+  this.storage.first = null;
+  this.count = 0;
+
 };
 
+Stack.prototype.push = function(value){
+	this.storage.value = value;
+	if(this.storage.last === null){
+		this.storage.last = this.storage.before;
+	}
+	this.storage.before = this.storage.last;
+	this.storage.last = this.storage.value;
+	this.count++;
+}
 
+Stack.prototype.pop = function(){
+	if(this.count === 0){
+		return this.count;
+	}
+	this.count--;
+	var peaceOut = this.storage.last;
+	this.storage.last = this.storage.before;
+	return seeYou;
+}
+
+Stack.prototype.size = function(){
+	return this.count;
+}
