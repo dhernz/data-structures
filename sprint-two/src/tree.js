8b4f4b7a9950e0d8b81311df58962c52af329356
dhernz <d.hernz@gmail.com>
2015-07-03 19:05:42 -0700

    adding some tree work

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,13 +1,17 @@
 var Tree = function(value){
-  var newTree = {};
+  var newTree = Object.create(treeMethods);
   newTree.value = value;
 
   newTree.lastValue = 0;
   // your code here
+  // newTree.root = null;
+  // newTree.parent = null.
   newTree.children = [{}];  // fix me
-  console.log(newTree.children);
-  newTree.addChild = treeMethods.addChild;
-  newTree.contains = treeMethods.contains;
+  // console.log(newTree.children);
+
+
+  // newTree.addChild = treeMethods.addChild;
+  // newTree.contains = treeMethods.contains;
   
   return newTree;
 
@@ -20,21 +24,52 @@ var Tree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-	this.children[0].value = value;
+	
+	// this.children[0] = this.addChild(value);
+
+	// _.each(this.children[0], function(newTree){
+	// // newTree.addChild(this.children[0].value);
+	//console.log(this.children.length);
+
+	// });
+
+	this.children[this.children.length -1].value = addChild(this.children);
+
+
 };
+
 
 treeMethods.contains = function(target){
 	//debugger;
 	var containsTarget = false;
-	for(var i = 0; i < this.children.length; i++){
-		for(var key in this.children[i]){
-			
-			if (this.children[i].value === target) {
+	
+	_.each(this.children, function(element){
+		_.each(element, function(keyValue, key){
+			if (keyValue === target) {
 				containsTarget = true;
 			}
-		}
-	}
+
+		});
+	});
+
+
+
 	return containsTarget;
+
+
+
+
+
+
+
+	// for(var i = 0; i < this.children.length; i++){
+	// 	for(var key in this.children[i]){
+			
+	// 		if (this.children[i].value === target) {
+	// 			containsTarget = true;
+	// 		}
+	// 	}
+	// }
 };
 
 
